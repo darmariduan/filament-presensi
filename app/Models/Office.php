c0be0e6588d832b93da12d758650a8597060de13
darmariduan <dharmarhiduwhan@gmail.com>
2025-02-22 11:50:41 +0700

    Add fillable properties and resource pages for Shift, Office, and Schedule models

--- app/Models/Office.php
+++ app/Models/Office.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Office extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'latitude',
+        'longitude',
+        'radius',
+    ];
 }
--- app/Models/Shift.php
+++ app/Models/Shift.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Shift extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'start_time',
+        'end_time',
+    ];
 }
