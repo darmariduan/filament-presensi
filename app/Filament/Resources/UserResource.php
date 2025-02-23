cc367f06d679b6cfdaf199bfaa8ddcad0eafe097
darmariduan <dharmarhiduwhan@gmail.com>
2025-02-23 22:29:50 +0700

    Add Filament Shield integration and role-based permissions for User, Shift, and Office models

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -35,7 +35,12 @@ class UserResource extends Resource
                 ->password()
                 ->dehydrateStateUsing(fn ($state) => Hash::make($state))
                 ->dehydrated(fn ($state) => filled($state))
-                ->required(fn (string $context): bool => $context === 'create')
+                ->required(fn (string $context): bool => $context === 'create'),
+                Forms\Components\Select::make('roles')
+                ->relationship('roles', 'name')
+                ->multiple()
+                ->preload()
+                ->searchable(),
             ]);
     }
 
@@ -46,6 +51,8 @@ class UserResource extends Resource
                 Tables\Columns\TextColumn::make('name')
                     ->searchable(),
                 Tables\Columns\TextColumn::make('email')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('roles.name')
                     ->searchable(),
                 Tables\Columns\TextColumn::make('email_verified_at')
                     ->dateTime()
